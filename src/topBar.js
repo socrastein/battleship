3580160041aadc63a22353a2c69c13870bd9fe8a
socrastein <31764357+socrastein@users.noreply.github.com>
2023-03-30 01:55:01 -0600

    append topBarItems to main topBar container

--- src/topBar.js
+++ src/topBar.js
@@ -3,21 +3,25 @@ import newGame from "./startGame";
 export default function createTopBar(){
   const topBarContainer = document.getElementById("topBarContainer");
   let game = newGame();
-  let playerOne = game.getFirstName;
-  let playerTwo = game.getSecondName;
+  let playerOne = game.getFirstName();
+  let playerTwo = game.getSecondName();
 
   const topBarGameTitle = document.createElement("div");
   topBarGameTitle.id = "topBarGameTitle";
   topBarGameTitle.classList.add("topBarItem");
   topBarGameTitle.innerHTML = "<h1>Battleship</h1>";
 
-  topBarLeftName = document.createElement("div");
+  const topBarLeftName = document.createElement("div");
   topBarLeftName.id = "topBarLeftName";
   topBarLeftName.classList.add("topBarItem");
   topBarLeftName.innerHTML = `<h2>${playerOne}</h2>`;
 
-  topBarRightName = document.createElement("div");
+  const topBarRightName = document.createElement("div");
   topBarRightName.id = "topBarRightName";
   topBarRightName.classList.add("topBarItem");
   topBarRightName.innerHTML = `<h2>${playerTwo}</h2>`;
+
+  topBarContainer.appendChild(topBarLeftName);
+  topBarContainer.appendChild(topBarGameTitle);
+  topBarContainer.appendChild(topBarRightName);
 }
