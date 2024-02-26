4bf0ee87144d330855ffe1c797936bd76212d229
socrastein <31764357+socrastein@users.noreply.github.com>
2024-02-26 11:19:08 -0700

    Build state, player and ship objects with basic functions

--- src/gameState.js
+++ src/gameState.js
@@ -1,21 +1,46 @@
-export default function gameState() {
-  const gameStages = [
-    "Name Selection", // 0
-    "Ship Placement", // 1
-    "Player Firing",  // 2
-    "Game Ending",    // 3
-  ];
+export const gameState = {
+  player1: undefined,
+  player2: undefined,
+  playerTurn: undefined,
+};
 
-  let gameStage = gameStages[0];
+const player = function (name) {
+  this.name = name;
+  this.ships = [];
+  this.shots = [];
+};
 
-  const logGameStage = () => {
-    console.log(gameStage);
-  };
+player.prototype.addShip = function (ship) {
+  this.ships.push(ship);
+};
 
-  const changeGameStage = (stageNumber) => {
-    gameStage = gameStages[stageNumber];
-    console.log(`Game Stage set to: ${gameStage}`);
+player.prototype.checkShot = function (square) {
+  this.ships.forEach((ship) => {
+    if (ship.includes(square)) {
+      ship.takeHit();
+      return true;
+    } else return false;
+  });
+};
+
+const ship = function (name, size) {
+  this.name = name;
+  this.size = size;
+  this.squares = [];
+  this.hits = 0;
+  this.isSunk = false;
+};
+
+ship.prototype.takeHit = function () {
+  this.hits += 1;
+  if (this.hits === this.size) {
+    this.isSunk = true;
   }
+};
 
-  return { gameStages, gameStage, logGameStage, changeGameStage };
-}
+ship.prototype.setLocation = function ([squares]) {
+  if (squares.length !== this.size) {
+    throw console.error("Ship needs to occupy as many squares as its size.");
+  }
+  this.squares = [...squares];
+};
