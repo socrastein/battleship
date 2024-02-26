25d0fb8f9b85469cc8644a50ade87ea3a3f80930
socrastein <31764357+socrastein@users.noreply.github.com>
2024-02-26 14:36:06 -0700

    Remove player object and incorporate into gameBoard

--- src/gameState.js
+++ src/gameState.js
@@ -1,29 +1,26 @@
-export const gameState = {
-  player1: undefined,
-  player2: undefined,
+const gameState = {
+  player1Name: undefined,
+  player2Name: undefined,
   playerTurn: undefined,
 };
 
-const player = function (name) {
-  this.name = name;
+const GameBoard = function (ownerName) {
+  this.owner = ownerName;
   this.ships = [];
-  this.shots = [];
-};
+  this.shotsFired = [];
+  this.shotsTaken = [];
+}
 
-player.prototype.addShip = function (ship) {
-  this.ships.push(ship);
-};
-
-player.prototype.checkShot = function (square) {
+GameBoard.prototype.receiveAttack = function (square) {
   this.ships.forEach((ship) => {
     if (ship.includes(square)) {
       ship.takeHit();
       return true;
     } else return false;
   });
-};
+}
 
-const ship = function (name, size) {
+const Ship = function (name, size) {
   this.name = name;
   this.size = size;
   this.squares = [];
@@ -31,16 +28,21 @@ const ship = function (name, size) {
   this.isSunk = false;
 };
 
-ship.prototype.takeHit = function () {
+Ship.prototype.takeHit = function () {
+  if(this.isSunk){
+    throw console.error("Ship has already been sunk.");
+  }
   this.hits += 1;
   if (this.hits === this.size) {
     this.isSunk = true;
   }
 };
 
-ship.prototype.setLocation = function ([squares]) {
+Ship.prototype.setLocation = function (squares) {
   if (squares.length !== this.size) {
     throw console.error("Ship needs to occupy as many squares as its size.");
   }
   this.squares = [...squares];
 };
+
+module.exports = { Ship, GameBoard }
