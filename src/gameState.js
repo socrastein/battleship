cc6a9ab3aec6583c8000a4276aa32e48af625525
socrastein <31764357+socrastein@users.noreply.github.com>
2024-02-26 16:57:08 -0700

    Add addShip, send/receive attack functions to GameBoard

--- src/gameState.js
+++ src/gameState.js
@@ -1,24 +1,57 @@
 const gameState = {
   player1Name: undefined,
+  player1Board: undefined,
+
   player2Name: undefined,
+  player2Board: undefined,
+
   playerTurn: undefined,
 };
+
+//Gameboard objects
 
 const GameBoard = function (ownerName) {
   this.owner = ownerName;
   this.ships = [];
   this.shotsFired = [];
   this.shotsTaken = [];
-}
+};
+
+GameBoard.prototype.addShip = function (name, size, locationArray) {
+  const ship = new Ship(name, size);
+  ship.setLocation(locationArray);
+  this.ships.push(ship);
+};
 
 GameBoard.prototype.receiveAttack = function (square) {
+  if (this.shotsTaken.includes(square)) {
+    throw console.error("This square has already been fired upon.");
+  }
+  this.shotsTaken.push(square);
   this.ships.forEach((ship) => {
     if (ship.includes(square)) {
       ship.takeHit();
       return true;
     } else return false;
   });
-}
+};
+
+GameBoard.prototype.sendAttack = function (gameboard, square) {
+  if (this.shotsFired.includes(square)) {
+    throw console.error("This square has already been fired upon.");
+  }
+  this.shotsFired.push(square);
+  gameboard.receiveAttack(square);
+};
+
+GameBoard.prototype.shipsAreSunk = function () {
+  this.ships.forEach((ship) => {
+    if (!ship.isSunk) return false;
+  });
+  return true;
+};
+
+//Ship objects
 
 const Ship = function (name, size) {
   this.name = name;
@@ -29,7 +62,7 @@ const Ship = function (name, size) {
 };
 
 Ship.prototype.takeHit = function () {
-  if(this.isSunk){
+  if (this.isSunk) {
     throw console.error("Ship has already been sunk.");
   }
   this.hits += 1;
@@ -45,4 +78,4 @@ Ship.prototype.setLocation = function (squares) {
   this.squares = [...squares];
 };
 
-module.exports = { Ship, GameBoard }
+module.exports = { Ship, GameBoard, gameState };
