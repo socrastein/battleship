86cba60b603fb6b6d96682bc6c8d3d67d314b6a9
socrastein <31764357+socrastein@users.noreply.github.com>
2024-02-26 21:48:54 -0700

    Create and style grid with column & row labels

--- src/display/gameGrid.js
+++ src/display/gameGrid.js
@@ -1,26 +1,74 @@
 import "./gameGrid.css";
-import squareClicked from "../squareClicked";
+import { squareClicked } from "../gameLoop";
 
-export default function createGrid() {
-  const letters = "ABCDEFGHIJ";
+const columnLetters = " ABCDEFGHIJ";
+
+export default function createGrid(small = false) {
   const gridContainer = document.createElement("div");
   gridContainer.id = "gridContainer";
 
-  for (let i = 0; i < 10; i++) {
-    let rowContainer = document.createElement("div");
-    rowContainer.id = `rowContainer${i + 1}`;
-    rowContainer.classList.add("rowContainer");
+  gridContainer.appendChild(createColumnLabels());
 
-    for (let j = 1; j <= 10; j++) {
-      let newGridSquare = document.createElement("div");
-      newGridSquare.classList.add("gridSquare");
-      newGridSquare.id = `${letters[i]}${j}`;
-      newGridSquare.innerText = newGridSquare.id;
-      newGridSquare.addEventListener("click", squareClicked);
-      rowContainer.appendChild(newGridSquare);
+  for (let i = 1; i <= 10; i++) {
+    const rowContainer = createRowContainer();
+    for (let j = 0; j <= 11; j++) {
+      if (j === 0 || j === 11) {
+        rowContainer.append(createRowLabel(columnLetters[i]));
+        continue;
+      }
+      rowContainer.append(createGridSquare(columnLetters[i], j));
     }
-    gridContainer.appendChild(rowContainer);
+    gridContainer.append(rowContainer);
   }
+
+  gridContainer.appendChild(createColumnLabels());
 
   return gridContainer;
 }
+
+const createRowContainer = function () {
+  const rowContainer = document.createElement("div");
+  rowContainer.classList.add("rowContainer");
+  return rowContainer;
+};
+
+//Row with ABCDEFGHIJ across top of grid
+const createColumnLabels = function () {
+  const rowContainer = createRowContainer();
+  for (let i = 0; i < 12; i++) {
+    const labelSquare = document.createElement("div");
+    labelSquare.classList.add("gridLabel");
+    if (i > 0 && i < 11) labelSquare.innerText = i;
+    rowContainer.append(labelSquare);
+  }
+  return rowContainer;
+};
+
+const createRowLabel = function (letter) {
+  const newGridSquare = document.createElement("div");
+  newGridSquare.classList.add("gridLabel");
+  newGridSquare.innerText = letter;
+
+  return newGridSquare;
+};
+
+const createGridSquare = function (column, row) {
+  const newGridSquare = document.createElement("div");
+  newGridSquare.classList.add("gridSquare");
+  newGridSquare.id = `${column}${row}`;
+  newGridSquare.addEventListener("click", () => {
+    squareClicked(newGridSquare.id);
+  });
+
+  return newGridSquare;
+};
+
+const showMiss = function (squareID) {
+  const square = document.getElementById(squareID);
+  square.classList.add("missedSquare");
+};
+
+const showHit = function (squareID) {
+  const square = document.getElementById(squareID);
+  square.classList.add("hitSquare");
+};
