552efe4558b529c790682292d1c239dbdd1e8780
socrastein <31764357+socrastein@users.noreply.github.com>
2024-02-27 20:40:23 -0700

    Check if on mobile and set state for drag & drop functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-import "./style.css"
+import "./style.css";
 import { createStartScreen } from "./display/startScreen";
+import { startGame } from "./gameLoop";
+import { gameState } from "./gameState";
 
 const mainContainer = document.getElementById("mainContainer");
 
-mainContainer.append(createStartScreen())
+const isMobile = () => {
+  if (
+    navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
+  ) {
+    return true;
+  } else return false;
+};
+
+if(isMobile){
+    gameState.isMobile = true;
+} else {
+    gameState.isMobile = false;
+}
+
+startGame(true);
