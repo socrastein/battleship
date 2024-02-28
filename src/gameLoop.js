67f2ba92de92bc8baac366ab88ec03004a0c1fa1
socrastein <31764357+socrastein@users.noreply.github.com>
2024-02-27 20:39:33 -0700

    Add initialization of grid and ships
    Add test parameter that auto-populates player names for quickly starting game

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -1,9 +1,27 @@
-import { gameState } from "./gameState";
+import { gameState, GameBoard } from "./gameState";
+import createGrid from "./display/gameGrid";
+import { placeableShips, setShipDimensions } from "./display/ships";
 
-export const startGame = function () {
+const mainContainer = document.getElementById("mainContainer");
+
+export const startGame = function (test = false) {
+  if (test) {
+    gameState.player1Name = "Matt";
+    gameState.player2Name = "Summer";
+  }
   console.log("Beginning new game!");
   console.log(`Player 1: ${gameState.player1Name}`);
   console.log(`Player 2: ${gameState.player2Name}`);
+  gameState.player1Board = new GameBoard(gameState.player1Name);
+  gameState.player1Board = new GameBoard(gameState.player2Name);
+
+  mainContainer.append(createGrid());
+  mainContainer.append(placeableShips())
+  setShipDimensions();
+
 };
 
+export const squareClicked = function (squareID) {
+  const square = document.getElementById(squareID);
+};
 const gameLoop = function () {};
