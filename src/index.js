216320f65774e331ee7063335c5ca237fa7cbd41
socrastein <31764357+socrastein@users.noreply.github.com>
2024-03-04 20:56:06 -0700

    Move isMobile to gameState and lock screen orientation to portrait

--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
 import "./style.css";
-import { createStartScreen } from "./display/startScreen";
-import { startGame } from "./gameLoop";
-import { gameState } from "./gameState";
-
-const mainContainer = document.getElementById("mainContainer");
-
-const isMobile = () => {
-  if (
-    navigator.userAgent.match(/Android/i) ||
-    navigator.userAgent.match(/webOS/i) ||
-    navigator.userAgent.match(/iPhone/i) ||
-    navigator.userAgent.match(/iPad/i) ||
-    navigator.userAgent.match(/iPod/i) ||
-    navigator.userAgent.match(/BlackBerry/i) ||
-    navigator.userAgent.match(/Windows Phone/i)
-  ) {
-    return true;
-  } else return false;
-};
-
-if(isMobile()){
-    gameState.isMobile = true;
-} else {
-    gameState.isMobile = false;
-}
-console.log("Mobile: " + gameState.isMobile);
-startGame(true);
+import { gameLoop } from "./gameLoop";
+const screenOrientation = window.screen.orientation;
+screenOrientation.lock("portrait")
+gameLoop();
