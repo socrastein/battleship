096cccea0d5640f5f2cf507cbadd8da012229441
socrastein <31764357+socrastein@users.noreply.github.com>
2024-03-06 18:47:40 -0700

    Remove screen orientation locks that don't seem to work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import "./style.css";
 import { gameLoop } from "./gameLoop";
-import { showEndScreen } from "./display/endScreen";
-const screenOrientation = window.screen.orientation;
-screenOrientation.lock("portrait")
-showEndScreen("Matt");
+gameLoop();
